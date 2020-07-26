5298b74c997d102cb7a5df5faca258fe5ce08e5a
mehamednews <mehamednews@gmail.com>
2020-07-27 00:24:52 +0100

    init

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const axios = require("axios").default;
 
+function wait(timeout = 61000) {
+	return Promise((r, j) => {
+		setTimeout(r, timeout);
+	});
+}
+
 (async function () {
 	for (let i = 0; i < 5000; i++) {
 		try {
 			const response = await axios.get(process.env.URL);
+			await wait();
 			console.log(i);
 		} catch (error) {
 			console.log(error);
