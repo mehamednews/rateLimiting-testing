6cb9f21d6125c30c40660794ca2b2909c3a31b51
mehamednews <mehamednews@gmail.com>
2020-09-26 18:17:48 +0100

    i

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const axios = require("axios").default;
+const https = require("https");
 
 function wait(timeout = 1000) {
-	return Promise((r, j) => {
+	return new Promise((r, j) => {
 		setTimeout(r, timeout);
 	});
 }
 
+// At request level
+const agent = new https.Agent({
+	rejectUnauthorized: false,
+});
+
 (async function () {
 	console.clear();
-	for (let i = 0; i < 5000; i++) {
+	for (let i = 0; i < 10000; i++) {
 		try {
-			const response = await axios.get(process.env.URL);
-			await wait();
-			console.log(i);
+			const response = await Promise.all(["a", "b", "c", "d", "e", "f"].map((el) => axios.get(`https://www.chewy.com/`, { httpsAgent: agent })));
+			console.log((i + 1) * 6);
 		} catch (error) {
-			console.log(error);
 			debugger;
 		}
 	}
